6f8115d17aa368fb44610804748433cd6c69a7a8
Mohanned Binmiskeen <mohn93@gmail.com>
2024-02-22 16:46:57 +0200

    fix params case

--- lib/cat.g.dart
+++ lib/cat.g.dart
@@ -9,21 +9,21 @@ part of 'cat.dart';
 Cat _$CatFromJson(Map<String, dynamic> json) => Cat(
       length: json['length'] as String,
       origin: json['origin'] as String,
-      imageLink: json['imageLink'] as String,
-      minWeight: json['minWeight'] as int,
-      maxWeight: json['maxWeight'] as int,
-      minLifeExpectancy: json['minLifeExpectancy'] as int,
-      maxLifeExpectancy: json['maxLifeExpectancy'] as int,
+      imageLink: json['image_link'] as String,
+      minWeight: json['min_weight'] as int,
+      maxWeight: json['max_weight'] as int,
+      minLifeExpectancy: json['min_life_expectancy'] as int,
+      maxLifeExpectancy: json['max_life_expectancy'] as int,
       name: json['name'] as String,
     );
 
 Map<String, dynamic> _$CatToJson(Cat instance) => <String, dynamic>{
       'length': instance.length,
       'origin': instance.origin,
-      'imageLink': instance.imageLink,
-      'minWeight': instance.minWeight,
-      'maxWeight': instance.maxWeight,
-      'minLifeExpectancy': instance.minLifeExpectancy,
-      'maxLifeExpectancy': instance.maxLifeExpectancy,
+      'image_link': instance.imageLink,
+      'min_weight': instance.minWeight,
+      'max_weight': instance.maxWeight,
+      'min_life_expectancy': instance.minLifeExpectancy,
+      'max_life_expectancy': instance.maxLifeExpectancy,
       'name': instance.name,
     };
